191eac1f9ddcd04dc0c0400a7d207b537ab8a6ca
Abhishek Shukla <91975183+abhis699@users.noreply.github.com>
2023-06-06 19:33:11 +0530

    Init RMX3371

--- anykernel.sh
+++ anykernel.sh
@@ -4,16 +4,16 @@
 ### AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=ExampleKernel by osm0sis @ xda-developers
+kernel.string=Agni Kernel by Abhishek
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=maguro
-device.name2=toro
-device.name3=toroplus
-device.name4=tuna
+device.name1=RMX3371
+device.name2=RE54E4L1
+device.name3=
+device.name4=
 device.name5=
 supported.versions=
 supported.patchlevels=
